6770befa96d84e3e20f049480c31c935a4774190
Sebastian <sstawecki@gmail.com>
2012-01-05 21:05:33 -0300

    DAY 10

---
+++ day10/js/common.js
@@ -0,0 +1,49 @@
+/*if(screen.width > 1200 ){
+
+	$('#content').css({
+		'-moz-column-count': '4',
+		'-moz-column-gap': '20px',
+		'-webkit-column-count': '4',
+		'-webkit-column-gap': '20px',
+		'column-count': '4',
+		'column-gap': '20px'
+	});
+	$('.mainImg').css({
+		'width': '300',
+		'height': 'auto'
+	});
+
+
+	
+}else{
+	
+	if(screen.width < 1200 && screen.width > 800 ){
+		$('#content').css({
+			'-moz-column-count': '3',
+			'-moz-column-gap': '20px',
+			'-webkit-column-count': '3',
+			'-webkit-column-gap': '20px',
+			'column-count': '3',
+			'column-gap': '20px'
+		});
+		$('.mainImg').css({
+			'width': '150',
+			'height': 'auto'
+		});		
+
+	
+	}else{
+		$('#content').css({
+			'-moz-column-count': '1',
+			'-moz-column-gap': '20px',
+			'-webkit-column-count': '1',
+			'-webkit-column-gap': '20px',
+			'column-count': '1',
+			'column-gap': '20px'
+		});
+		$('.mainImg').css({
+			'display':'none'
+		});		
+	}
+	
+}*/
